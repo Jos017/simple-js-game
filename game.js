1601c4c5ca014113c0f6312345621c30865e4f44
Jose Rios <jrbernabe@gmail.com>
2022-12-30 17:11:29 -0400

    Add responsive to the canvas rendering

--- game.js
+++ game.js
@@ -1,11 +1,32 @@
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+let canvasSize;
 
 function startGame() {
-  let canvasSize;
+  // Define cell size from canvas;
+  const elementsSize = canvasSize / 10;
 
+  // Configure size of elements in board
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'start';
+  game.textBaseline = 'bottom';
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      game.fillText(
+        emojis['X'],
+        elementsSize * i - elementsSize * 0.15,
+        elementsSize * j + elementsSize
+      );
+    }
+  }
+}
+
+function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
   } else {
@@ -15,22 +36,5 @@ function startGame() {
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
 
-  const elementsSize = canvasSize / 10;
-
-  game.font = elementsSize + 'px Verdana';
-  game.textAlign = 'start';
-  game.textBaseline = 'bottom';
-  console.log(game);
-  console.log(elementsSize);
-
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      game.fillRect(elementsSize * i, elementsSize * j, elementsSize, elementsSize);
-      game.fillText(
-        emojis['X'],
-        elementsSize * i - elementsSize * 0.15,
-        elementsSize * j + elementsSize
-      );
-    }
-  }
+  startGame();
 }
